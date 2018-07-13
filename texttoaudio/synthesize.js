7cd2ea7bb3bd34ef66a1a2c72eb75b1adae004b6
Ben <ben.harloe@gmail.com>
2018-07-12 19:42:59 -0700

    Text to audio

---
+++ texttoaudio/synthesize.js
@@ -0,0 +1,27 @@
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+AWS.config.update({ region: 'us-east-1' });
+
+/* Synthesizes plain text or SSML into a file of human-like speech. */
+var polly = new AWS.Polly({ apiVersion: "2016-06-10" });
+
+var params = {
+    OutputFormat: "mp3",
+    SampleRate: "8000",
+    Text: "All Gaul is divided into three parts",
+    TextType: "text",
+    VoiceId: "Joanna"
+};
+
+polly.synthesizeSpeech(params, function (err, data) {
+    if (err) {
+        console.log(err, err.stack);
+    } else {
+        console.log(data); 
+        fs.writeFile('audio.mp3', data.AudioStream, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }
+});
